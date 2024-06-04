7a9a0a6f9b45f8a3d40090f3d25bba0cd8599e5b
Michael <fatlycka@gmail.com>
2024-06-04 21:08:50 +0300

    create simple ui

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,21 @@ import { renderDeck } from "./deck.js"
 function getCardDeck(){
     const deck = renderDeck()
     // deck.forEach(el => document.body.append(el))
+    document.querySelector('.dealer-cards').append(deck[51])
+    document.querySelector('.dealer-cards').append(deck[50])
+    document.querySelector('.dealer-score').innerText = +deck[51].children[0].dataset.value + +deck[50].children[0].dataset.value
+    document.querySelector('.player-cards').append(deck[0])
+    document.querySelector('.player-cards').append(deck[7])
+    document.querySelector('.player-score').innerText = +deck[0].children[0].dataset.value + +deck[7].children[0].dataset.value
     return deck
 }
-
 getCardDeck()
+
+// everything runs inside it
+const main = () => {
+
+}
+
+main()
+
+
